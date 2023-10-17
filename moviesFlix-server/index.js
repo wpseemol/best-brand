cd22c880ef968c29759596e9e157111e6df67218
wpseemol <wpseemol@gmail.com>
2023-10-17 23:05:21 +0600

    data base mongodb user name or password keep on env file and connect database withe server

--- moviesFlix-server/index.js
+++ moviesFlix-server/index.js
@@ -4,18 +4,45 @@ const express = require('express');
 //come from cors.
 const cors = require('cors');
 
-const app = express();
+require('dotenv').config();
 
-//come from dotenv.
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+//come from dotenv.
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.s0x7bvc.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db('admin').command({ ping: 1 });
+        console.log(
+            'Pinged your deployment. You successfully connected to MongoDB!'
+        );
+    } finally {
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (request, response) => {
     response.send('this is testing server');
 });
 
 app.listen(port, () => {
-    console.log('Server is running on Port' + port);
+    console.log('Server is running on Port' + port );
 });
