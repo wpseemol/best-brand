245124cb76bd21c029840f93231238c7d6035358
wpseemol <wpseemol@gmail.com>
2023-10-17 22:28:01 +0600

    movie flex site react and react router install with talwindcss

---
+++ moviesFlix/src/main/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <div className='bg-slate-600 text-center'>this is fresh react</div>
+    </React.StrictMode>
+);
