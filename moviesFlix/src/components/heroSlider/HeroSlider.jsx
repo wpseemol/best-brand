54265921439642a4e1a8d07e61d43060b0065989
wpseemol <wpseemol@gmail.com>
2023-10-18 22:09:06 +0600

    slider custom zoom efect add

--- moviesFlix/src/components/heroSlider/HeroSlider.jsx
+++ moviesFlix/src/components/heroSlider/HeroSlider.jsx
@@ -9,15 +9,24 @@ const HeroSlider = ({ children }) => {
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setCounter((counter) => (counter + 1) % 11);
-        }, 500);
+            setCounter((prevCounter) => {
+                let num = parseInt(prevCounter, 10) + 1;
+                if (num < 10) {
+                    return `0${num}`;
+                } else if (num <= 50) {
+                    return `${num}`;
+                } else {
+                    return '00';
+                }
+            });
+        }, 1000);
 
         return () => {
             clearInterval(interval);
         };
     }, []);
 
-    console.log(counter);
+    console.log('1.0' + counter);
 
     return (
         <>
@@ -27,7 +36,7 @@ const HeroSlider = ({ children }) => {
                 effect={'fade'}
                 centeredSlides={true}
                 autoplay={{
-                    delay: 5000,
+                    delay: 10000,
                     disableOnInteraction: false,
                 }}
                 pagination={{
@@ -42,18 +51,22 @@ const HeroSlider = ({ children }) => {
                             <img
                                 src="https://i.ibb.co/mXngkTk/Nice-Chair-4.jpg"
                                 alt=""
-                                className={`w-full h-full object-cover object-center duration-300 scale-[1.${counter}]`}
+                                className="w-full h-full object-cover object-center 
+                                duration-1000"
+                                style={{ transform: `scale(1.${counter})` }}
                             />
                         </div>
                     </div>
                 </SwiperSlide>
-                {/* <SwiperSlide>
+                <SwiperSlide>
                     <div className="w-full h-screen">
                         <div className=" overflow-hidden">
                             <img
                                 src="https://i.ibb.co/KjGxLYK/Nice-Chair-5.jpg"
-                                className="w-full h-full object-cover object-center"
+                                className="w-full h-full object-cover object-center 
+                                duration-1000"
                                 alt=""
+                                style={{ transform: `scale(1.${counter})` }}
                             />
                         </div>
                     </div>
@@ -63,8 +76,10 @@ const HeroSlider = ({ children }) => {
                         <div className=" overflow-hidden">
                             <img
                                 src="https://i.ibb.co/D9Nm7fc/product-12-2.jpg"
-                                className="w-full h-full object-cover object-center"
+                                className="w-full h-full object-cover object-center 
+                                duration-1000"
                                 alt=""
+                                style={{ transform: `scale(1.${counter})` }}
                             />
                         </div>
                     </div>
@@ -74,12 +89,14 @@ const HeroSlider = ({ children }) => {
                         <div className=" overflow-hidden">
                             <img
                                 src="https://i.ibb.co/8zdyY5V/product-12-3.jpg"
-                                className="w-full h-full object-cover object-center"
+                                className="w-full h-full object-cover object-center 
+                                duration-1000"
                                 alt=""
+                                style={{ transform: `scale(1.${counter})` }}
                             />
                         </div>
                     </div>
-                </SwiperSlide> */}
+                </SwiperSlide>
                 ...
             </Swiper>
 
