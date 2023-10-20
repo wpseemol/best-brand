ddba90e46ae40d13c4a0ec0525669bcff5f08bb2
wpseemol <wpseemol@gmail.com>
2023-10-20 11:13:18 +0600

    some desigin add on userinfo

--- moviesFlix/src/components/loginInformation/LoginInformation.jsx
+++ moviesFlix/src/components/loginInformation/LoginInformation.jsx
@@ -16,14 +16,14 @@ const LoginInformation = ({ info, logOutFun }) => {
     return (
         <>
             <div className="absolute right-0 bg-gray-600/80 text-white rounded-md">
-                <div className="w-fit sm:m-5 m-2 mt-4">
-                    <ul className=" text-base font-medium">
+                <div className="w-fit sm:m-5 m-2 mt-4 flex flex-col items-end sm:gap-4 gap-1">
+                    <ul className="sm:text-lg text-base font-medium">
                         <li className="mt-2">{info?.displayName}</li>
-                        <li className="sm:my-3">{info?.email}</li>
+                        <li className="mt-2">{info?.email}</li>
                     </ul>
                     <button
                         onClick={handalLogout}
-                        className="secondaryBtn rounded-md sm:mt-3">
+                        className="secondaryBtn rounded-md border border-white p-2 hover:bg-slate-400 duration-150">
                         Log Out
                     </button>
                 </div>
